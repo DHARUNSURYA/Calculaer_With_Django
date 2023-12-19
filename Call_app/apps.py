f00f828e5f91194640621236df30b9c42a6f731f
Dharunsurya <surya@gmail.com>
2023-12-19 08:54:19 +0530

    First commit

---
+++ Call_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CallAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Call_app'
---
+++ Call_app/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import re
+# Create your views here.
+def greetings(request):
+    res=render(request,'index.html')
+    return res
+
+def calculation(request):
+    if request.method=="POST":
+        values=request.POST['values'] #Get a value using name attribute in inputbox a value was string("03+3+3")
+        print(values)
+        vals=re.findall(r"(\d+)",values) #extrect values that means it get only value with our operators using re.findall()['03','3','3']
+        print(vals)
+        operators=['+','x','÷','-','.','%']
+        opr=[]
+        for v in values:  # to get operatro value
+            for o in operators:
+                if v==o:
+                    opr.append(o)
+        print(opr)                      #['+','+']
+        print(re.findall(r"(\d+)",values))
+
+        for o in opr:
+            if o=='.':
+                i=opr.index(o)
+                res=vals[i]+"."+vals[i+1]
+                vals.remove(vals[i+1])
+                opr.remove(opr[i])
+                vals[i]=res
+                print(vals)
+                print(opr)
+        for o in opr:
+            if o=='%':
+                i=opr.index(o)
+                res=(float(vals[i])/100)*float(vals[i+1])
+                vals.remove(vals[i+1])
+                opr.remove(opr[i])
+                vals[i]=res
+                print(vals)
+                print(opr)
+        for o in opr:
+            if o=='÷':
+                i=opr.index(o)
+                res=float(vals[i])/float(vals[i+1])
+                vals.remove(vals[i+1])
+                opr.remove(opr[i])
+                vals[i]=str(res)
+                print(vals)
+                print(opr)
+        for o in opr:
+            if o=='x':
+                i=opr.index(o)
+                res=float(vals[i])*float(vals[i+1])
+                vals.remove(vals[i+1])
+                opr.remove(opr[i])
+                vals[i]=str(res)
+                print(vals)
+                print(opr)
+        for o in opr:
+            if o=='+':
+                i=opr.index(o)
+                res=float(vals[i])+float(vals[i+1])
+                vals.remove(vals[i+1])
+                opr.remove(opr[i])
+                vals[i]=str(res)
+                print(vals)
+                print(opr)
+            if o=='-':
+                i=opr.index(o)
+                res=float(vals[i])-float(vals[i+1])
+                vals.remove(vals[i+1])
+                opr.remove(opr[i])
+                vals[i]=str(res)
+                print(vals)
+                print(opr)
+
+      
+        if len(opr)!=0:
+            if opr[0]=='÷':
+                result = float(vals[0])/float(vals[1])
+            elif opr[0]=='x':
+                result = float(vals[0])*float(vals[1])
+            elif opr[0]=='+':
+                result = float(vals[0])+float(vals[1])
+            else :
+                result = float(vals[0])-float(vals[1])
+        else:
+            result = float(vals[0])
+
+        final_result=round(result,2)
+        print(final_result)
+       
+    return render(request,'index.html',{'result':final_result,'values':values})
